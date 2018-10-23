182f7a1911b7503b8d2c0e15a7bcd0b512a4cb8c
Geomit <Geomit@LAPTOP>
2018-10-23 21:28:10 +0300

    Diavazei ta stoixeia tou xrhsth

--- BMI2/Program.cs
+++ BMI2/Program.cs
@@ -10,6 +10,15 @@ namespace BMI2
     {
         static void Main(string[] args)
         {
+            Console.Write("Please give me your weight:");
+            string w = Console.ReadLine();
+            float weight = float.Parse(w);
+            Console.Write("Please give me your height:");
+            string h = Console.ReadLine();
+            float height = float.Parse(h);
+            float bmi = weight / (height * height);
+            Console.WriteLine($"BMI is: {bmi}");
+            Console.Read();
         }
     }
 }
